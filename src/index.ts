9e3f731c9c4728ad689a60a75358547d1ce6e182
scintilla4evr <kacper17@outlook.com>
2022-05-29 23:43:38 +0200

    Added basic beeping

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
-import { readFileSync } from "fs"
+import { readFileSync, writeFileSync } from "fs"
 import { join } from "path"
-import { renderPattern } from "./render"
+import WavEncoder, { AudioData } from "wav-encoder"
+import { renderPattern, sampleRate } from "./render"
 import { SynthesizerData } from "./types"
 
 const score = JSON.parse(
     readFileSync(join(__dirname, "../data/example.json"), "utf8")
 ) as SynthesizerData
+
+const wavBuffer = renderPattern(score.patterns["test"], score)
+
+const wavEncoderData: AudioData = {
+    sampleRate: sampleRate,
+    channelData: [wavBuffer, wavBuffer]
+}
+
+WavEncoder.encode(wavEncoderData).then(wavBuffer => {
+    writeFileSync(
+        join(__dirname, "../data/pattern.wav"),
+        Buffer.from(wavBuffer),
+        "utf-8"
+    )
+})
--- src/render.ts
+++ src/render.ts
@@ -1,11 +1,49 @@
-import { SynthesizerData, SynthesizerPattern } from "./types"
-import { getPatternTime } from "./util"
+import { SynthesizerData, SynthesizerPattern, SynthesizerNote } from "./types"
+import { getBeatTime, getPatternTime } from "./util"
 
-const sampleRate = 44100
+export const sampleRate = 44100
+
+type SynthesizerRenderNote = {
+    frequency: number,
+    start: number,
+    end: number
+}
+
+function processNotes(notes: SynthesizerNote[], data: SynthesizerData): SynthesizerRenderNote[] {
+    const beatTime = getBeatTime(data)
+
+    return notes.map(note => {
+        return {
+            frequency: 440,
+            start: note.start * beatTime,
+            end: (note.start + note.length) * beatTime
+        }
+    })
+}
 
 export function renderPattern(pattern: SynthesizerPattern, data: SynthesizerData) {
     const length = Math.floor(getPatternTime(pattern, data) * sampleRate)
     const buffer = new Float32Array(length)
+
+    const notes = processNotes(pattern.notes, data)
+
+    const synth = {
+        note: (note: SynthesizerRenderNote, time: number) => {
+            if (time < note.start) {
+                return 0
+            } else if (time >= note.end) {
+                return 0
+            }
+
+            return Math.sin((time - note.start) * note.frequency * 2 * Math.PI)
+        }
+    }
+
+    for (let i = 0; i < length; i++) {
+        const time = i / sampleRate
+
+        buffer[i] = notes.reduce((acc, note) => acc + synth.note(note, time), 0)
+    }
 
     return buffer
 }
